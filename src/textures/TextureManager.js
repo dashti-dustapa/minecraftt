c280e984d6fcb70fc9294a61a2f775ce1db4b3c0
dashti-dustapa <rahandpshtiwan2011@gmail.com>
2026-09-13 17:45:56 -0700

    Update TextureManager.js

--- src/textures/TextureManager.js
+++ src/textures/TextureManager.js
@@ -1,18 +1,19 @@
 /**
  * TextureManager.js
- * Procedural 16x16 pixel-art texture generator for Minecraft blocks.
+ * Procedural 16x16 pixel textures, block materials, and getBlockMaterial resolver.
  */
 
-const THREE = window.THREE;
+import * as THREE from 'three';
 
 export class TextureManager {
     constructor() {
         this.textures = {};
         this.materials = {};
-        this.generateTextures();
+        this.initTextures();
+        this.initMaterials();
     }
 
-    createPixelCanvas(drawFn) {
+    createPixelTexture(drawFn) {
         const canvas = document.createElement('canvas');
         canvas.width = 16;
         canvas.height = 16;
@@ -26,19 +27,17 @@ export class TextureManager {
         return texture;
     }
 
-    generateTextures() {
-        // 1. Dirt Texture
-        this.textures.dirt = this.createPixelCanvas((ctx) => {
+    initTextures() {
+        this.textures.dirt = this.createPixelTexture(ctx => {
             ctx.fillStyle = '#866043';
             ctx.fillRect(0, 0, 16, 16);
-            for (let i = 0; i < 45; i++) {
+            for (let i = 0; i < 40; i++) {
                 ctx.fillStyle = Math.random() > 0.5 ? '#68482f' : '#9b7454';
                 ctx.fillRect(Math.floor(Math.random() * 16), Math.floor(Math.random() * 16), 1, 1);
             }
         });
 
-        // 2. Grass Top Texture
-        this.textures.grass_top = this.createPixelCanvas((ctx) => {
+        this.textures.grass_top = this.createPixelTexture(ctx => {
             ctx.fillStyle = '#5b8c32';
             ctx.fillRect(0, 0, 16, 16);
             for (let i = 0; i < 40; i++) {
@@ -47,26 +46,19 @@ export class TextureManager {
             }
         });
 
-        // 3. Grass Side Texture
-        this.textures.grass_side = this.createPixelCanvas((ctx) => {
-            // Dirt base
+        this.textures.grass_side = this.createPixelTexture(ctx => {
             ctx.fillStyle = '#866043';
             ctx.fillRect(0, 0, 16, 16);
             for (let i = 0; i < 35; i++) {
                 ctx.fillStyle = Math.random() > 0.5 ? '#68482f' : '#9b7454';
                 ctx.fillRect(Math.floor(Math.random() * 16), Math.floor(Math.random() * 16), 1, 1);
             }
-            // Grass overlay on top
             ctx.fillStyle = '#5b8c32';
             ctx.fillRect(0, 0, 16, 4);
-            for (let x = 0; x < 16; x++) {
-                const drip = Math.floor(Math.random() * 3);
-                ctx.fillRect(x, 4, 1, drip);
-            }
+            for (let x = 0; x < 16; x++) ctx.fillRect(x, 4, 1, Math.floor(Math.random() * 3));
         });
 
-        // 4. Stone Texture
-        this.textures.stone = this.createPixelCanvas((ctx) => {
+        this.textures.stone = this.createPixelTexture(ctx => {
             ctx.fillStyle = '#787878';
             ctx.fillRect(0, 0, 16, 16);
             for (let i = 0; i < 50; i++) {
@@ -75,38 +67,95 @@ export class TextureManager {
             }
         });
 
-        // 5. Wood Plank Texture
-        this.textures.plank = this.createPixelCanvas((ctx) => {
+        this.textures.cobble = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#6c6c6c';
+            ctx.fillRect(0, 0, 16, 16);
+            ctx.fillStyle = '#3a3a3a';
+            for (let y = 0; y < 16; y += 4) ctx.fillRect(0, y, 16, 1);
+            for (let i = 0; i < 35; i++) {
+                ctx.fillStyle = Math.random() > 0.5 ? '#4f4f4f' : '#8a8a8a';
+                ctx.fillRect(Math.floor(Math.random() * 16), Math.floor(Math.random() * 16), 2, 1);
+            }
+        });
+
+        this.textures.plank = this.createPixelTexture(ctx => {
             ctx.fillStyle = '#9c7849';
             ctx.fillRect(0, 0, 16, 16);
             ctx.fillStyle = '#6b4f2c';
             ctx.fillRect(0, 3, 16, 1);
             ctx.fillRect(0, 7, 16, 1);
             ctx.fillRect(0, 11, 16, 1);
-            ctx.fillRect(0, 15, 16, 1);
         });
 
-        // 6. Cobblestone Texture
-        this.textures.cobblestone = this.createPixelCanvas((ctx) => {
-            ctx.fillStyle = '#6c6c6c';
+        this.textures.log_side = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#6d5332';
             ctx.fillRect(0, 0, 16, 16);
-            ctx.fillStyle = '#3a3a3a';
-            for (let y = 0; y < 16; y += 4) {
-                ctx.fillRect(0, y, 16, 1);
-            }
+            ctx.fillStyle = '#48361f';
+            for (let x = 0; x < 16; x += 4) ctx.fillRect(x, 0, 2, 16);
+        });
+
+        this.textures.log_top = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#8f6f47';
+            ctx.fillRect(0, 0, 16, 16);
+            ctx.fillStyle = '#6d5332';
+            ctx.fillRect(3, 3, 10, 10);
+            ctx.fillStyle = '#48361f';
+            ctx.fillRect(6, 6, 4, 4);
+        });
+
+        this.textures.torch = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#6b4f2c';
+            ctx.fillRect(6, 4, 4, 12);
+            ctx.fillStyle = '#ffaa00';
+            ctx.fillRect(6, 1, 4, 3);
+            ctx.fillStyle = '#ffffff';
+            ctx.fillRect(7, 2, 2, 2);
+        });
+
+        this.textures.glass = this.createPixelTexture(ctx => {
+            ctx.clearRect(0, 0, 16, 16);
+            ctx.strokeStyle = '#c4e3f3';
+            ctx.lineWidth = 1;
+            ctx.strokeRect(0, 0, 16, 16);
+            ctx.fillStyle = '#e8f7ff';
+            ctx.fillRect(3, 3, 2, 2);
+            ctx.fillRect(10, 10, 3, 1);
+        });
+
+        this.textures.water = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#2e5ec4';
+            ctx.fillRect(0, 0, 16, 16);
             for (let i = 0; i < 30; i++) {
-                ctx.fillStyle = Math.random() > 0.5 ? '#4f4f4f' : '#8a8a8a';
+                ctx.fillStyle = Math.random() > 0.5 ? '#3b6ddb' : '#23499e';
                 ctx.fillRect(Math.floor(Math.random() * 16), Math.floor(Math.random() * 16), 2, 1);
             }
         });
 
-        // Setup materials
-        this.materials.dirt = new THREE.MeshLambertMaterial({ map: this.textures.dirt });
-        this.materials.stone = new THREE.MeshLambertMaterial({ map: this.textures.stone });
-        this.materials.plank = new THREE.MeshLambertMaterial({ map: this.textures.plank });
-        this.materials.cobblestone = new THREE.MeshLambertMaterial({ map: this.textures.cobblestone });
+        this.textures.diamond = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#787878';
+            ctx.fillRect(0, 0, 16, 16);
+            for (let i = 0; i < 40; i++) {
+                ctx.fillStyle = Math.random() > 0.5 ? '#5e5e5e' : '#8c8c8c';
+                ctx.fillRect(Math.floor(Math.random() * 16), Math.floor(Math.random() * 16), 1, 1);
+            }
+            ctx.fillStyle = '#4cedd9';
+            for (let i = 0; i < 16; i++) {
+                ctx.fillRect(Math.floor(Math.random() * 14) + 1, Math.floor(Math.random() * 14) + 1, 2, 2);
+            }
+        });
 
-        // Grass block requires multi-face materials: [East, West, Top, Bottom, North, South]
+        this.textures.crafting_table = this.createPixelTexture(ctx => {
+            ctx.fillStyle = '#9c7849';
+            ctx.fillRect(0, 0, 16, 16);
+            ctx.fillStyle = '#5c4028';
+            ctx.strokeRect(1, 1, 14, 14);
+            ctx.fillStyle = '#3a2412';
+            ctx.fillRect(4, 4, 3, 8);
+            ctx.fillRect(9, 4, 3, 8);
+        });
+    }
+
+    initMaterials() {
         this.materials.grass = [
             new THREE.MeshLambertMaterial({ map: this.textures.grass_side }),
             new THREE.MeshLambertMaterial({ map: this.textures.grass_side }),
@@ -115,5 +164,40 @@ export class TextureManager {
             new THREE.MeshLambertMaterial({ map: this.textures.grass_side }),
             new THREE.MeshLambertMaterial({ map: this.textures.grass_side })
         ];
+
+        this.materials.dirt = new THREE.MeshLambertMaterial({ map: this.textures.dirt });
+        this.materials.stone = new THREE.MeshLambertMaterial({ map: this.textures.stone });
+        this.materials.cobble = new THREE.MeshLambertMaterial({ map: this.textures.cobble });
+        this.materials.plank = new THREE.MeshLambertMaterial({ map: this.textures.plank });
+        this.materials.log = [
+            new THREE.MeshLambertMaterial({ map: this.textures.log_side }),
+            new THREE.MeshLambertMaterial({ map: this.textures.log_side }),
+            new THREE.MeshLambertMaterial({ map: this.textures.log_top }),
+            new THREE.MeshLambertMaterial({ map: this.textures.log_top }),
+            new THREE.MeshLambertMaterial({ map: this.textures.log_side }),
+            new THREE.MeshLambertMaterial({ map: this.textures.log_side })
+        ];
+        this.materials.torch = new THREE.MeshBasicMaterial({ map: this.textures.torch, transparent: true });
+        this.materials.glass = new THREE.MeshLambertMaterial({ map: this.textures.glass, transparent: true, opacity: 0.45 });
+        this.materials.water = new THREE.MeshLambertMaterial({ map: this.textures.water, transparent: true, opacity: 0.65 });
+        this.materials.diamond = new THREE.MeshLambertMaterial({ map: this.textures.diamond });
+        this.materials.crafting_table = new THREE.MeshLambertMaterial({ map: this.textures.crafting_table });
+    }
+
+    getBlockMaterial(blockId) {
+        switch (blockId) {
+            case 0: return this.materials.grass;
+            case 1: return this.materials.dirt;
+            case 2: return this.materials.stone;
+            case 3: return this.materials.cobble;
+            case 4: return this.materials.plank;
+            case 5: return this.materials.torch;
+            case 6: return this.materials.glass;
+            case 7: return this.materials.water;
+            case 8: return this.materials.diamond;
+            case 9: return this.materials.log;
+            case 10: return this.materials.crafting_table;
+            default: return this.materials.stone;
+        }
     }
 }
